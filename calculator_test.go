ec8e87a4ab69f2736837a5d9a16b7f302f45b77c
uni51 <unieye51@gmail.com>
2023-12-06 08:59:01 +0900

    17. Go言語におけるテストの基本

---
+++ calculator_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	result := Add(1, 2)
+	expected := 3
+	if result != expected {
+		t.Errorf("Add(1, 2) failed. Expected 3, got %d", result)
+	}
+}
---
+++ main.go
@@ -0,0 +1,6 @@
+package main
+
+func main() {
+	result := Add(1, 2)
+	println(result)
+}
