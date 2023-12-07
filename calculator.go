afc24d27a5f750ec06b9aa3bf86415ce2206c5e7
uni51 <unieye51@gmail.com>
2023-12-07 12:44:04 +0900

    18. テストをより深ぼる

--- calculator.go
+++ calculator.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 func Add(a, b int) int {
 	return a + b
+}
+
+func Divide(a, b int) (int, error) {
+	if b == 0 {
+		return 0, fmt.Errorf("Cannot divide by zero")
+	}
+	return a / b, nil
 }
--- calculator_test.go
+++ calculator_test.go
@@ -1,6 +1,10 @@
 package main
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestAdd(t *testing.T) {
 	result := Add(1, 2)
@@ -8,4 +12,17 @@ func TestAdd(t *testing.T) {
 	if result != expected {
 		t.Errorf("Add(1, 2) failed. Expected 3, got %d", result)
 	}
+}
+
+func TestDivide(t *testing.T) {
+	result, err := Divide(6, 3)
+	assert.NoError(t, err, "an unexpected error occured: %v", err)
+	assert.Equal(t, 2, result, "they should be equal")
+
+	// if err != nil {
+	// 	t.Errorf("Divide(6, 3) failed. Got error %s", err.Error())
+	// }
+	// if result != 2 {
+	// 	t.Errorf("Divide(6, 3) failed. Expected 2, got %d", result)
+	// }
 }
